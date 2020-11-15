e364dd440fb4831965f9e881f34937541f0c4165
Ojus Doshi <ojus.doshi@gmail.com>
2020-11-15 16:43:46 -0500

    something twitter
    blah

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-console.dir(document);
+//how can I change the color of all bars to blue?
+
+var bars = document.querySelectorAll(".bar");
+bars.forEach(bar => {
+  //console.dir(bar);
+  bar.style.backgroundColor = "blue";
+  bar.style.borderRadius = "100%";
+});
+
+var myContainer = document.querySelector('section');
+console.dir(myContainer);
+myContainer.style.borderWidth = "10px";
+myContainer.style.borderColor = "rgb(244,0,0)";
+myContainer.style.borderStyle = "solid";
+myContainer.style.width = "80%";
